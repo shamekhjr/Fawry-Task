f83505e7ef3703f6fa0912b6295294068b0c240d
Mohamed Shamekh <shamekhjr@gmail.com>
2025-07-05 00:39:29 +0300

    Added logic for checkout

--- src/services/ShippingService.java
+++ src/services/ShippingService.java
@@ -1,6 +1,7 @@
 package services;
 
 import models.Customer;
+import models.Product;
 
 public class ShippingService {
     private double shippingRate;
@@ -24,12 +25,33 @@ public class ShippingService {
         if (weight < 0.0) {
             throw new IllegalArgumentException("Weight must not be a negative number.");
         }
-        return weight * shippingRate;
+        return weight * shippingRate; // assuming shippingRate is per kg
     }
 
 
 
     public void checkout(Customer customer) {
+        double totalPrice = customer.getCart().getTotalPrice();
+        double totalWeight = customer.getCart().getTotalWeight() / 1000;
 
+        System.out.println("** Shipment notice **");
+        for (Product product: customer.getCart().getProducts()) {
+            if (product.isShippable()) {
+                System.out.println(product.getPurchasedQuantity() +"x " + product.getName()+"      "+product.getWeight() * product.getPurchasedQuantity()+"g");
+            }
+        }
+        System.out.println("Total package weight: " + customer.getCart().getTotalWeight() / 1000 + "kg");
+        System.out.println();
+        System.out.println();
+
+
+        System.out.println("** Checkout receipt **");
+        for (Product product: customer.getCart().getProducts()) {
+            System.out.println(product.getPurchasedQuantity() + "x " + product.getName() + "      " + product.getPrice() * product.getPurchasedQuantity());
+        }
+        System.out.println("-------------------------");
+        System.out.println("Subtotal: " + totalPrice);
+        System.out.println("Shipping: " + calculateShippingCost(totalWeight));
+        System.out.println("Amount: "+ (totalPrice + totalWeight));
     }
 }
